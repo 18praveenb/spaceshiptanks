5d57c7b4255f2246190396d91feaf05e553add0f
Praveen <Praveen@Shire-2.local>
2015-05-22 09:45:15 -0700

    fixed the error messages
    but still no bullets

--- code.js
+++ code.js
@@ -137,38 +137,12 @@ function circ(x, y, radius, color, underSpaceship){
 }
 
 /* Move bullet */
-function moveBullet(bullet){
+function moveBullets(){
     for(var i = 0; i < dgid("scene").childNodes.length; i++){
-        if(ga(dgid("scene").childNodes[i],"class")=="bullet"){
+        if((scene.childNodes[i].type == 1) && (ga(dgid("scene").childNodes[i],"class").slice(0, 5)=="bullet")){
             sa(dgid("scene").childNodes[i],"x",(ga(dgid("scene").childNodes[i],"x")+ga(dgid("scene").childNodes[i],"vx")));
         }
     }
-}
-
-function enumerate(array, block) {
-    for (var i=0; i<array.length; ++i) {
-        /* The block can return a boolean value. Returning false ends enumeration. */
-        var shouldContinue = block(array[i]);
-        if (shouldContinue == false) {break;}
-    }
-}
-
-function enumerateChildNodes(childNodes, block) {
-    function checkingBlock(node) {
-        /* Element nodes (like tiles and units) have node type 1. Some other node types cause crashes when they are accessed with common enumeration functions. */
-        if (node.nodeType == 1) {
-            return block(node);
-        }
-    }
-    enumerate(childNodes, checkingBlock);
-}
-
-function stringOfPropertiesOfObject(object) {
-    var str = "";
-    for (key in object) {
-        str += key + ": " + object[key] + ", ";
-    }
-    return str;
 }
 
 /*** Nodes ***/
@@ -254,7 +228,7 @@ function update(){
     sa(dgid("node_p2"),"x",ga(p2.node,"x"));
     sa(dgid("node_p2"),"y",ga(p2.node,"y"));
     
-    moveBullet();
+    moveBullets();
 }
 
 
